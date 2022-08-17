1579f4c80b3c0f7eb80e9cedfa1219a3e2086b54
Jose Hernandez <jhernandez_456@yahoo.com>
2022-08-16 18:23:45 -0700

    Server is set up, ready for models and routes to be executed

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const { expressjwt } = require('express-jwt')
+
+process.env.SECRET
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+mongoose.connect('mongodb://localhost:27017/rtv',
+() => console.log("Data Base Connected.")
+)
+
+app.use('/rtv', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] }))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({ errMsg: err.message })
+})
+
+app.listen(9000, () => {
+    console.log("Server - LocalHost//9000 - Connected")
+})
